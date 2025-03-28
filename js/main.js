696c404edd1e08d56fb55e8b1a1690408c2f1112
slyncrafty <101752232+slyncrafty@users.noreply.github.com>
2025-03-28 18:46:21 +0000

    Added calculator representation in html and basic arithmetic functionality in javascript.

---
+++ js/main.js
@@ -0,0 +1,55 @@
+const displayScreen = document.querySelector('#input');
+const keys = document.querySelector('.keys');
+
+// get key values as array
+let keyInputs = [];
+let result;
+
+keys.addEventListener('click', e => {
+    if(e.target.matches('button')){
+        readKeys(e);
+    }
+});
+
+function readKeys (e) {
+    const key = e.target;
+    const keyValue = key.textContent;
+    const keyAction = key.dataset.action; 
+    const displayedNum = displayScreen.textContent;
+    if(keyValue === 'C'){
+        reset();
+    }
+    else if(keyAction === 'add' ||
+            keyAction === 'subtract' ||
+            keyAction === 'multiply' || 
+            keyAction === 'divide'
+    ){
+        // console.log(keyAction);
+        displayScreen.textContent = keyValue;
+        keyInputs.push(keyValue);
+    }
+    else if(keyValue === '='){
+        displayScreen.textContent =  calculate(result);
+        keyInputs = [];
+    }
+    else{
+        if(!keyAction){
+            keyInputs.push(keyValue);
+            result = keyInputs.join('');
+            displayScreen.textContent = result;
+        }
+    }
+}
+
+
+function calculate(keys){
+    return eval(keys);
+}
+
+function reset(){
+    keyInputs = [];
+    result = '';
+    displayScreen.textContent = "";
+}
+
+
